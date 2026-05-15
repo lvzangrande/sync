332772d93c1f923d0d018d696126a23b9bee08ca
ogmukaaa <samuel.s.santos26@aluno.senai.br>
2026-05-15 16:57:28 -0300

    Criação de Formulário

---
+++ suporte.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Suporte Sync</title>
+</head>
+<body>
+
+    <div class="box-intro">
+        <h1>SINCRONIZANDO SOLUÇÕES: <br>
+            SUPORTE TÉCNICO ESPECIALIZADO</h1>
+
+        <p>
+            Relate problemas em sistemas mecatrônicos ou tire duvidas <br>
+            com nossa equipe de especialistas
+        </p>
+    </div>
+
+
+
+    <article class="card-canal">
+        <div class="icon-container">
+    </div>
+        <h3>TEMPO DE RESPOSTA</h3>
+        <p>Resposta em até 2h</p>
+    </article>
+
+
+
+    <div class="box-form">
+        <span>Fale com um especialista</span>
+        <label>Nome Completo</label>
+        <input type="text" name="nome" placeholder="Nome Completo">
+
+        <label>E-mail</label>
+        <input type="email" name="email" placeholder="E-mail">
+
+        <label>Telefone</label>
+        <input type="number" name="telefone" placeholder="Telefone">
+
+        <label>Descrição de Problema/Duvida</label>
+        <input type="text" name="descricao" placeholder="Relate sua duvida">
+    </div>
+</body>
+</html>
