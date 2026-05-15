36fb57a7851f02b90166c180f10fb892122f5d29
SILAS POSSARLE CARVALHO SILVA <possarle.silas16@gmail.com>
2026-05-15 16:14:12 -0300

    Criação da section com titulo

--- catalago_profissionais.php
+++ catalago_profissionais.php
@@ -1,21 +1,48 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Catalago</title>
 </head>
+
 <body>
+
+    <section>
+        <span>Rede de Especialistas</span>
+        <h1>Catalogo de Profissionais</h1>
+        <p>Sincronize sua operação com técnicos mecatrônicos certificados e disponíveis em tempo real.</p>
+        <select name="" id="">
+            <option value="">Ordernar: Melhor Avaliação</option>
+            <option value="">Ordernar: Maior Preço</option>
+            <option value="">Ordernar: Menor Preço</option>
+        </select>
+    </section>
+
+
+
     <div class="card">
+
         <div class="disponibilidade">Disponivel</div>
+
         <div class="avaliacao">4.9</div>
-        <img src="https://static.vecteezy.com/ti/fotos-gratis/t2/57068323-solteiro-fresco-vermelho-morango-em-mesa-verde-fundo-comida-fruta-doce-macro-suculento-plantar-imagem-foto.jpg" alt="">
+
+        <img src="https://static.vecteezy.com/ti/fotos-gratis/t2/57068323-solteiro-fresco-vermelho-morango-em-mesa-verde-fundo-comida-fruta-doce-macro-suculento-plantar-imagem-foto.jpg"alt="">
+
         <p>Nome Profissional</p>
-        <p>Descrição Profissional</p>
+
+        <p>Especialidade Profissional</p>
+
         <span>Tempo de empresa</span>
+
         <span>Quantidade de Trabalhos</span>
-        <p>Preço do serviço  <p>por dia</p></p>
+
+        <p>Preço do serviço</p>
+        <p>por dia</p>
+        
         <a href="./contratar profissional">Contatra Profissional</a>
     </div>
 </body>
+
 </html>
