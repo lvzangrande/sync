145295bff12a4f716a04f1cd309a48494f152d24
SILAS POSSARLE CARVALHO SILVA <possarle.silas16@gmail.com>
2026-05-15 15:58:33 -0300

    Criação do card do Profissional

---
+++ catalago_profissionais.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Catalago</title>
+</head>
+<body>
+    <div class="card">
+        <div class="disponibilidade">Disponivel</div>
+        <div class="avaliacao">4.9</div>
+        <img src="https://static.vecteezy.com/ti/fotos-gratis/t2/57068323-solteiro-fresco-vermelho-morango-em-mesa-verde-fundo-comida-fruta-doce-macro-suculento-plantar-imagem-foto.jpg" alt="">
+        <p>Nome Profissional</p>
+        <p>Descrição Profissional</p>
+        <span>Tempo de empresa</span>
+        <span>Quantidade de Trabalhos</span>
+        <p>Preço do serviço  <p>por dia</p></p>
+        <a href="./contratar profissional">Contatra Profissional</a>
+    </div>
+</body>
+</html>
