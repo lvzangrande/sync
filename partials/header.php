5b5bde39ea6c019283391ba00c190f71d250d735
elolobs <eloahlourbar@gmail.com>
2026-05-15 16:54:51 -0300

    add codigo

---
+++ partials/header.php
@@ -0,0 +1,15 @@
+<header>
+    <nav>
+        <ul class="menu-superior">
+            <div class="logo">
+                <img src="">
+            </div>
+            <ul>
+                <li><a href="./home.php">Início</a></li>
+                <li><a href="./home.php#Tecnologia">Nossa Tecnologia</a></li>
+                <li><a href="#">Profissionais</a></li>
+                <li><a href="#">Equipe</a></li>
+                <li><a href="#">Suporte</a></li>
+
+            </ul>
+</header>
