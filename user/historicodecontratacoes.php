8fe3db89d4f03e68a6df3ad8328a4ebef6530fac
lvzangrande <zangrande360@gmail.com>
2026-05-19 15:07:25 -0300

    Criei e iniciei a estilização da página de historico de contratações

---
+++ user/historicodecontratacoes.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Histórico de contratações</title>
+    <link rel="stylesheet" href="../css/userpage.css">
+</head>
+<header>
+    <a class="botaovoltar" href="userpage.php"><img href="botaovoltar.png" width='200'></a>
+</header>
+<body>
+    <table>
+        <tr>
+            <th colspan="99">Histórico de contratações<th>
+        </tr>
+        <tr>
+            <td>
+                <a>joaolinux</a>
+                <a>11/09</a>
+                <a class='verdetalhes' href=''>Ver detalhes</a>
+            </td>
+        </tr>
+                <tr>
+            <td>
+                <a>joaolinux</a>
+            </td>
+        </tr>        <tr>
+            <td>
+                <a>joaolinux</a>
+            </td>
+        </tr>        <tr>
+            <td>
+                <a>joaolinux</a>
+            </td>
+        </tr>   
+    <table>
+
+</body>
+</html>
--- user/userpage.php
+++ user/userpage.php
@@ -13,7 +13,7 @@
     <?php require '../php/saudacao.php';?>
     <img src=../img/joia.jfif>
     <h1>Zé</h1>
-    <a class="historico" href="">Ver histórico de contratações</a>
+    <a class="historico" href="historicodecontratacoes.php">Ver histórico de contratações</a>
     <footer>
         <p>Cadastrado desde de 2026</p>
     </footer>
