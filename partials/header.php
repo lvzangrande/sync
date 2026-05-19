f3f567cc38a97c0415c3256367ca52c78cac136f
elolobs <eloahlourbar@gmail.com>
2026-05-19 16:55:46 -0300

    add codigo

--- partials/header.php
+++ partials/header.php
@@ -1,17 +1,23 @@
 <header>
     <nav>
-        <ul class="menu-superior">
+        <div class="menu-superior">
             <div class="logo">
-                <img src="">
+                <img src="imagens/logosemfundo.png" class="logo">
             </div>
-            <ul>
+            <ul class="nav-links">
                 <li><a href="./home.php">Início</a></li>
                 <li><a href="./home.php#Tecnologia">Nossa Tecnologia</a></li>
-                <li><a href="#">Profissionais</a></li>
-                <li><a href="#">Equipe</a></li>
-                <li><a href="#">Suporte</a></li>
-                <li><a href="#">Entrar</a></li>
-                <li><a href="#">Cadastro</a></li>
-
+                <li><a href="./catalogo_profissionais.php">Profissionais</a></li>
+                <li><a href="./equipe.php">Equipe</a></li>
+                <li><a href="./suporte.php">Suporte</a></li>
             </ul>
+
+            <div class="buttons">
+                <a class="btn-entrar" href="#">Entrar</a>
+                <a class="btn-cadastro" href="#">Começar <span class="material-symbols-outlined">
+                        arrow_outward
+                    </span></a>
+            </div>
+        </div>
+    </nav>
 </header>
