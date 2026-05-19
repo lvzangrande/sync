35060237147f4876aad9bb943b327bad5937eb92
lvzangrande <zangrande360@gmail.com>
2026-05-19 14:40:46 -0300

    Iniciei a estilização da página do usuário

--- user/userpage.php
+++ user/userpage.php
@@ -4,11 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Seu perfil</title>
+    <link rel="stylesheet" href="../css/userpage.css">
 </head>
 <body>
     <header>
         <a></a>
     </header>
-    <?php require '../php/saudacao.php'?>
+    <?php require '../php/saudacao.php';?>
+    <img src=../img/joia.jfif>
+    <h1>Zé</h1>
+    <a class="historico" href="">Ver histórico de contratações</a>
+    <footer>
+        <p>Cadastrado desde de 2026</p>
+    </footer>
 </body>
 </html>
