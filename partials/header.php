4b0f0ab5bd8ccf0a0680bfefd3811782c52b2e09
elolobs <eloahlourbar@gmail.com>
2026-05-20 14:24:10 -0300

    add codigo

--- partials/header.php
+++ partials/header.php
@@ -5,20 +5,11 @@
                 <img src="imagens/logosemfundo.png" class="logo">
             </div>
             <ul class="nav-links">
-                <li><a href="./home.php">Início</a></li>
-                <li><a href="./home.php#Tecnologia">Nossa Tecnologia</a></li>
-<<<<<<< HEAD
+                <li><a href="./inicio.php">Início</a></li>
+                <li><a href="./inicio.php#Tecnologia">Nossa Tecnologia</a></li>
                 <li><a href="./catalogo_profissionais.php">Profissionais</a></li>
                 <li><a href="./equipe.php">Equipe</a></li>
                 <li><a href="./suporte.php">Suporte</a></li>
-=======
-                <li><a href="#">Profissionais</a></li>
-                <li><a href="#">Equipe</a></li>
-                <li><a href="#">Suporte</a></li>
-                <li><a href="login.php">Entrar</a></li>
-                <li><a href="cadastro.php">Cadastro</a></li>
-
->>>>>>> 1c4e7af2ecd97b66db89d8d01f9044cbfda81993
             </ul>
 
             <div class="buttons">
