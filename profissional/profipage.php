19623d874256b397c02c8bf176714e6d5d8d456d
lvzangrande <zangrande360@gmail.com>
2026-05-20 17:04:07 -0300

    Finalizei a função de saudação no title

--- profissional/profipage.php
+++ profissional/profipage.php
@@ -1,10 +1,35 @@
+<?php require_once '../crud.php';
+session_start();
+
+if (!isset($_SESSION['autenticado'])) {
+    header("Location: ../login.php");
+    exit();
+}
+
+function nomeUsuario() {
+    if (isset($_SESSION['nome'])) {
+        $nomeCompleto = trim($_SESSION['nome']);
+        
+        $palavras = explode(" ", $nomeCompleto);
+        
+        $duasPalavras = array_slice($palavras, 0, 2);
+        
+        $nomeEncurtado = implode(" ", $duasPalavras);
+
+        echo htmlspecialchars($nomeEncurtado);
+    } else {
+        echo "Usuário";
+    }
+}
+
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/profipage.css">
-    <title>Seu perfil</title>
+    <title>Olá <?=nomeUsuario($pdo);?></title>
 </head>
 <body>
     <?php
