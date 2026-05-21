e9d3d4552d7ae6264fa43f21b2fce1b7bb2ec264
SILAS POSSARLE CARVALHO SILVA <possarle.silas16@gmail.com>
2026-05-21 14:53:15 -0300

    Fazendo que a pagina de contrato mande direto pra pagina de segundo_contrato e removendo o arquivo da SESSION

---
+++ user/segundo_contrato.php
@@ -0,0 +1,74 @@
+<?php
+
+require_once '../crud.php';
+session_start();
+
+$_SESSION['tipo_servico'] = $_POST['tipo_serv'];
+$_SESSION['descricao_problema'] = $_POST['desc'];
+$_SESSION['data'] = $_POST['data'];
+$_SESSION['tempo_planejado'] = $_POST['tempo'];
+$_SESSION['endereco_servico'] = $_POST['end_serv'];
+$_SESSION['idcard'] = $_POST['id_profissional'];
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/contratar.css">
+    <title>Contratar</title>
+</head>
+
+<body>
+    <main>
+        <!-- ----- Card Funcionario ----- -->
+        <section class="perfil-profissional">
+
+            <a href="./catalogo_profissionais.php" class="voltar">
+                ← Voltar ao Catálogo
+            </a>
+            <?php
+            echo '
+                <div class="card-profissional">
+
+                    <!-- Foto -->
+                    <div class="foto-profissional">
+                        <img src="' . $profissional['img_user'] . '"
+                            alt="">
+                    </div>
+
+                    <!-- Informações -->
+                    <div class="info-profissional">
+
+                        <div class="topo-info">
+                            <h1>' . $profissional['nome'] . '</h1>
+                            <span class="disponibilidade">' . $profissional['status'] . '</span>
+                        </div>
+
+                        <h2>' . $profissional['especialidade'] . '</h2>
+
+                        <div class="meta-info">
+                            <span class="avaliacao">' . $profissional['notas'] . '</span>
+                            <span>(247 trabalhos)</span>
+                            <span>12 anos</span>
+                        </div>
+                    </div>
+
+                    <!-- Preço -->
+                    <div class="preco-servico">
+                        <span class="valor">R$' . $profissional['valor_dia'] . '</span>
+                        <span class="periodo">/dia</span>
+                    </div>
+
+                </div> 
+                ';
+            ?>
+
+
+        </section>
+    </main>
+</body>
+
+</html>
