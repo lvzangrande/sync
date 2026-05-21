a440bacee5e98ea5b495ed3e1448e4373bb4861b
lvzangrande <zangrande360@gmail.com>
2026-05-21 16:49:15 -0300

    Iniciei a personalização dos placeholders no formulário de editar dados pessoais

--- user/editardados.php
+++ user/editardados.php
@@ -1,4 +1,6 @@
 <?php 
+require_once '../crud.php';
+
 if (session_status() === PHP_SESSION_NONE){
     session_start();
 }
@@ -6,12 +8,6 @@ if (!isset($_SESSION['autenticado'])) {
     header("Location: ../login.php");
     exit();
 }
-
-$_SESSION['id_user'] = $usuario_banco['id_user'];
-$_SESSION['nome']    = $usuario_banco['nome'];
-$_SESSION['tipo']    = $usuario_banco['categoria'];
-$_SESSION['foto']    = $usuario_banco['img_user'];
-
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -22,14 +18,23 @@ $_SESSION['foto']    = $usuario_banco['img_user'];
     <title>Editar dados pessoas</title>
 </head>
 <body>
-<?php require_once '../partials/header.php';?>
+<?php require_once '../partials/header.php';
+
+$_SESSION['autenticado'] = true;
+$_SESSION['id_user']     = $usuario_banco['id_user'];
+$_SESSION['nome']        = $usuario_banco['nome'];
+$_SESSION['tipo']        = $usuario_banco['categoria'];
+$_SESSION['foto']        = $usuario_banco['img_user'];
+$_SESSION['cpfCnpj']     = $usuario_banco['cpf_cnpj'];
+$_SESSION['tel']         = $usuario_banco['telefone'];
+?>
 
 
     <h2>Editar Dados Pessoais</h2>
     <div class='formulario'>
         <form action="#" method="POST" class="form-editar-usuario">
 
-        <input type="text" name="nome" placeholder="Nome completo" required>
+        <input type="text" name="nome" placeholder="<?=$_SESSION['nome']?>" required>
 
         <input type="email" name="email" placeholder="E-mail" required>
 
