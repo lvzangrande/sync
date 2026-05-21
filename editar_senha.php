7b2d27c883e42e4e2b256d1aef4b27162bd5c7d5
guiisilva00 <guilherme.p.silva51@aluno.senai.br>
2026-05-21 14:59:16 -0300

    Criação do esqueleto da página de edição de senha.

--- editar_senha.php
+++ editar_senha.php
@@ -47,7 +47,46 @@ if (isset($_POST['senha_atual']) && isset($_POST['nova_senha']) && isset($_POST[
 </head>
 
 <body>
+<div class="login-container"> <h2>Alterar Senha</h2>
+    <p class="subtitle">Mantenha a segurança da sua conta Sync atualizada.</p>
 
+    <?php if (!empty($erro)): ?>
+        <div class="alert alert-erro">
+            <?php echo $erro; ?>
+        </div>
+    <?php endif; ?>
+
+    <?php if (!empty($sucesso)): ?>
+        <div class="alert alert-sucesso">
+            <?php echo $sucesso; ?>
+        </div>
+    <?php endif; ?>
+
+    <form action="editar_senha.php" method="POST">
+        
+        <div class="form-group">
+            <label for="senha_atual">Senha Atual</label>
+            <input type="password" id="senha_atual" name="senha_atual" class="input-control" placeholder="Digite sua senha atual" required>
+        </div>
+
+        <div class="form-separacao">
+            
+            <div class="form-group">
+                <label for="nova_senha">Nova Senha</label>
+                <input type="password" id="nova_senha" name="nova_senha" class="input-control" placeholder="Nova senha" required>
+            </div>
+
+            <div class="form-group">
+                <label for="confirma_senha">Confirmar Nova Senha</label>
+                <input type="password" id="confirma_senha" name="confirma_senha" class="input-control" placeholder="Repita a nova senha" required>
+            </div>
+
+        </div>
+
+        <button type="submit" class="btn-submit">Atualizar Senha</button>
+        
+    </form>
+</div>
 </body>
 
 </html>
