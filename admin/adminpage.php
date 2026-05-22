262da48dfdfd6b452e0103d367ff4c135e1273ac
guiisilva00 <guilherme.p.silva51@aluno.senai.br>
2026-05-22 15:09:52 -0300

    Começando a editar a página de admin.

--- admin/adminpage.php
+++ admin/adminpage.php
@@ -1,19 +1,19 @@
-<?php require_once '../crud.php';
+<?php
 session_start();
+require_once '../crud.php';
+
 
 if (!isset($_SESSION['autenticado'])) {
     header("Location: ../login.php");
     exit();
 }
 
-function nomeUsuario() {
+function nomeUsuario()
+{
     if (isset($_SESSION['nome'])) {
         $nomeCompleto = trim($_SESSION['nome']);
-        
         $palavras = explode(" ", $nomeCompleto);
-        
         $duasPalavras = array_slice($palavras, 0, 2);
-        
         $nomeEncurtado = implode(" ", $duasPalavras);
 
         echo htmlspecialchars($nomeEncurtado);
@@ -24,15 +24,25 @@ function nomeUsuario() {
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Olá <?=nomeUsuario($pdo);?></title>
+    <title>Olá <?php nomeUsuario(); ?>!</title>
+    <link rel="stylesheet" href="css/partials.css">
+    <link rel="stylesheet" href="css/home.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Google+Sans:ital,opsz,wght@0,17..18,400..700;1,17..18,400..700&family=Lexend+Deca:wght@100..900&family=Lexend:wght@100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 </head>
+
 <body>
-    <header>
-        <a></a>
-    </header>
-    <?php require '../php/saudacao.php'?>
+    <?php require_once '../partials/header.php'; ?>
+    <main>
+
+    </main>
+    <?php require_once '../partials/footer.php'; ?>
 </body>
+
 </html>
