968685eb366014be875e06cdc046a2a9abb0f45f
guiisilva00 <guilherme.p.silva51@aluno.senai.br>
2026-05-25 16:12:22 -0300

    seila

--- crud.php
+++ crud.php
@@ -6,7 +6,7 @@ $port = 3306;
 $dbname = "db_sync";
 $username = "root";
 $password = "";
-
+    
 try {
     $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
