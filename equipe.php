fd8241ca9d052b24322c9250f71882855a21513c
ogmukaaa <samuel.s.santos26@aluno.senai.br>
2026-05-26 16:47:01 -0300

    Atualização da pagina de equipe

--- equipe.php
+++ equipe.php
@@ -1,11 +1,126 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/partials.css">
+    <link rel="stylesheet" href="./css/equipe.css">
     <title>Nossa Equipe</title>
 </head>
 <body>
+    <header>
+        <?php include 'partials/header.php'; ?>
+    </header>
     
+    <main class="container-equipe">
+        <h1 class="titulo">Nossa equipe</h1>
+        
+        <div class="grid-equipe">
+            
+            <div class="card-equipe">
+                <div class="foto-container">
+                     <img src="foto-silas" alt="Silas Porssales" class="foto-img">
+                </div>
+                <h3 class="dev-name">Silas Porssales</h3>
+                <h4 class="dev-funcao">Gerente Back-End</h4>
+                
+                <hr class="card-divider">
+                
+                <div class="section-heading"></div>
+                <p class="dev-text"></p>
+                
+                <div class="section-heading"></div>
+                <ul class="contribuicao">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+
+            <div class="card-equipe">
+                <div class="foto-container">
+                    <img src="foto-lucas.jpg" alt="Lucas Zangrande" class="foto-img">
+                </div>
+                <h3 class="dev-name">Lucas Zangrande</h3>
+                <h4 class="dev-funcao">Gerente de Projeto e Fullstack</h4>
+                
+                <hr class="card-divider">
+                
+                <div class="section-heading"></div>
+                <p class="dev-text"></p>
+                
+                <div class="section-heading"></div>
+                <ul class="contribuicao">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+
+            <div class="card-equipe">
+                <div class="foto-container">
+                    <img src="foto-eloah.jpg" alt="Eloah Barbosa" class="foto-img">
+                </div>
+                <h3 class="dev-name">Eloah Barbosa</h3>
+                <h4 class="dev-funcao">Gerente Front-End</h4>
+                
+                <hr class="card-divider">
+                
+                <div class="section-heading"></div>
+                <p class="dev-text"></p>
+                
+                <div class="section-heading"></div>
+                <ul class="contribuicao">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+            
+            <div class="card-equipe">
+                <div class="foto-container">
+                    <img src="foto-guilherme.jpg" alt="Guilherme de Paula" class="foto-img">
+                </div>
+                <h3 class="dev-name">Guilherme de Paula</h3>
+                <h4 class="dev-funcao">Auxiliar Back-End</h4>
+                
+                <hr class="card-divider">
+                
+                <div class="section-heading"></div>
+                <p class="dev-text"></p>
+                
+                <div class="section-heading"></div>
+                <ul class="contribuicao">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+
+            <div class="card-equipe">
+                <div class="foto-container">
+                    <img src="foto-samuel.jpg" alt="Samuel de Sousa" class="foto-img">
+                </div>
+                <h3 class="dev-name">Samuel de Sousa</h3>
+                <h4 class="dev-funcao">Auxiliar Front-End</h4>
+                
+                <hr class="card-divider">
+                
+                <div class="section-heading"></div>
+                <p class="dev-text"></p>
+                
+                <div class="section-heading"></div>
+                <ul class="contribuicao">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                
+                </ul>
+            </div>
+
+        </div>
+    </main>
+
+    <?php include 'partials/footer.php'; ?>
 </body>
 </html>
