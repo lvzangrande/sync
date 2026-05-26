ad579b020167ae4de36a6688d15eadac200226e6
SILAS POSSARLE CARVALHO SILVA <possarle.silas16@gmail.com>
2026-05-26 14:43:06 -0300

    Redefinindo os caminhos para um url basica

--- partials/header.php
+++ partials/header.php
@@ -1,4 +1,9 @@
-<link rel="stylesheet" href="../css/partials.css">
+<?php
+define('BASE_URL', 'http://localhost/sync/');
+$base = BASE_URL;
+?>
+
+<link rel="stylesheet" href="<?= $base?>css/partials.css">
 <header>
     <nav>
         <div class="menu-superior">
@@ -6,16 +11,16 @@
                 <img src="imagens/logosemfundo.png" class="logo">
             </div>
             <ul class="nav-links">
-                <li><a href="./inicio.php">Início</a></li>
-                <li><a href="./inicio.php#Tecnologia">Tecnologia</a></li>
-                <li><a href="catalogo_profissionais.php">Profissionais</a></li>
-                <li><a href="./equipe.php">Equipe</a></li>
-                <li><a href="./suporte.php">Suporte</a></li>
+                <li><a href="<?= $base?>inicio.php">Início</a></li>
+                <li><a href="<?= $base?>inicio.php#Tecnologia">Tecnologia</a></li>
+                <li><a href="<?= $base?>catalogo_profissionais.php">Profissionais</a></li>
+                <li><a href="<?= $base?>equipe.php">Equipe</a></li>
+                <li><a href="<?= $base?>suporte.php">Suporte</a></li>
             </ul>
 
             <div class="buttons">
-                <a class="btn-entrar" href="./login.php">Entrar</a>
-                <a class="btn-cadastro" href="./cadastro.php">Começar <span class="material-symbols-outlined">arrow_outward</span></a>
+                <a class="btn-entrar" href="<?= $base?>login.php">Entrar</a>
+                <a class="btn-cadastro" href="<?= $base?>cadastro.php">Começar <span class="material-symbols-outlined">arrow_outward</span></a>
             </div>
         </div>
     </nav>
