eb50fc2e4e878a37e9cefd46457fe01d2f1cf041
SAMUEL DE SOUSA DOS SANTOS <25254297@educ123.sp.senai.br>
2026-05-28 15:23:21 -0300

    Ajustes no css da pagina

--- equipe.php
+++ equipe.php
@@ -22,18 +22,17 @@
                     <img src="" alt="Eloah Barbosa" class="card-img">
                 </div>
                 <div class="card-info-basic">
-                    <span class="badge-front">DEV FRONT-END</span>
+                    <span class="badge-funcao">DEV FRONT-END</span>
                     <h3 class="dev-name">Eloah Barbosa</h3>
                 </div>
                 <div class="card-hover-panel">
-                    <span class="badge-front">DEV FRONT-END</span>
+                    <span class="badge-funcao">DEV FRONT-END</span>
                     <h3 class="dev-name-hover">Eloah Barbosa</h3>
                     <p class="dev-contribution">
-                        Responsável pela criação da interface responsiva, componentização do front system e aplicação das transições e efeitos em CSS.
+
                     </p>
                     <div class="social-links">
-                        <i class="fab fa-linkedin"></i>
-                        <i class="fab fa-github"></i>
+                        
                     </div>
                 </div>
             </div>
@@ -43,18 +42,17 @@
                     <img src="" alt="" class="card-img">
                 </div>
                 <div class="card-info-basic">
-                    <span class="badge-back">DEV BACK-END</span>
-                    <h3 class="dev-name"></h3>
+                    <span class="badge-funcao">DEV BACK-END</span>
+                    <h3 class="dev-name">Silas Porsales</h3>
                 </div>
                 <div class="card-hover-panel">
-                    <span class="badge-back">DEV BACK-END</span>
+                    <span class="badge-funcao">DEV BACK-END</span>
                     <h3 class="dev-name-hover">Silas Porsales</h3>
                     <p class="dev-contribution">
-                        Contribuiu de forma crucial na modelagem do banco de dados (MySQL), desenvolvendo as rotas da API em PHP e integrando o sistema de autenticação segura.
+
                     </p>
                     <div class="social-links">
-                        <i class="fab fa-linkedin"></i>
-                        <i class="fab fa-github"></i>
+                        
                     </div>
                 </div>
             </div>
@@ -64,60 +62,58 @@
                     <img src="" alt="Samuel de Sousa" class="card-img">
                 </div>
                 <div class="card-info-basic">
-                    <span class="badge-front">DEV FRONT-END</span>
+                    <span class="badge-funcao">DEV FRONT-END</span>
                     <h3 class="dev-name">Samuel de Sousa</h3>
                 </div>
                 <div class="card-hover-panel">
-                    <span class="badge-front">DEV FRONT-END</span>
+                    <span class="badge-funcao">DEV FRONT-END</span>
                     <h3 class="dev-name-hover">Samuel de Sousa</h3>
                     <p class="dev-contribution">
-                        Desenhou o protótipo de alta fidelidade no Figma, definiu a paleta de cores, tipografia e mapeou a jornada de experiência do usuário.
+                    
                     </p>
                     <div class="social-links">
-                        <i class="fab fa-linkedin"></i>
-                        <i class="fab fa-behance"></i>
+                        
+            
                     </div>
                 </div>
             </div>
 
             <div class="team-card">
                 <div class="card-image-wrapper">
-                    <img src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?w=400" alt="Luca Silva" class="card-img">
+                    <img src="" alt="Lucas Zangrande" class="card-img">
                 </div>
                 <div class="card-info-basic">
-                    <span class="badge-back">DEV BACK-END</span>
-                    <h3 class="dev-name">Luca Silva</h3>
+                    <span class="badge-funcao">Gerente de Projeto</span>
+                    <h3 class="dev-name">Lucas Zangrande</h3>
                 </div>
                 <div class="card-hover-panel">
-                    <span class="badge-back">DEV BACK-END</span>
-                    <h3 class="dev-name-hover">Luca Silva</h3>
+                    <span class="badge-funcao">DEV BACK-END</span>
+                    <h3 class="dev-name-hover">Lucas Zangrande</h3>
                     <p class="dev-contribution">
-                        Estruturou as regras de negócio em PHP, configurou os servidores de hospedagem e garantiu o desempenho das consultas do site.
+
                     </p>
                     <div class="social-links">
-                        <a href="#" target="_blank"><i class="fab fa-linkedin"></i></a>
-                        <a href="#" target="_blank"><i class="fab fa-github"></i></a>
+
                     </div>
                 </div>
             </div>
 
             <div class="team-card">
                 <div class="card-image-wrapper">
-                    <img src="https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?w=400" alt="Mariana Souza" class="card-img">
+                    <img src="" alt="" class="card-img">
                 </div>
                 <div class="card-info-basic">
-                    <span class="badge-pm">PROJECT MANAGER</span>
-                    <h3 class="dev-name">Mariana Souza</h3>
+                    <span class="badge-funcao">DEV BACK-END</span>
+                    <h3 class="dev-name">Guilherme de Paula</h3>
                 </div>
                 <div class="card-hover-panel">
-                    <span class="badge-pm">PROJECT MANAGER</span>
-                    <h3 class="dev-name-hover">Mariana Souza</h3>
+                    <span class="badge-funcao">DEV BACK-END</span>
+                    <h3 class="dev-name-hover">Guilherme de Paula</h3>
                     <p class="dev-contribution">
-                        Coordenou os prazos das sprints, organizou as entregas do grupo e validou os requisitos do sistema junto às regras do projeto.
+
                     </p>
                     <div class="social-links">
-                        <a href="#" target="_blank"><i class="fab fa-linkedin"></i></a>
-                        <a href="#" target="_blank"><i class="fab fa-github"></i></a>
+                        
                     </div>
                 </div>
             </div>
