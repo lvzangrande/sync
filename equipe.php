978dacfed8e526a8a79b88fdcde2803e4ecb328f
ogmukaaa <samuel.s.santos26@aluno.senai.br>
2026-05-29 14:50:15 -0300

    Arrumando header da pagina

--- equipe.php
+++ equipe.php
@@ -1,30 +1,35 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nossa Equipe</title>
+    
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    
+    <link href="https://fonts.googleapis.com/css2?family=Barlow+Semi+Condensed:ital,wght@0,100..900;1,100..900&family=Google+Sans:ital,opsz,wght@0,17..18,400..700;1,17..18,400..700&family=Lexend+Deca:wght@100..900&family=Lexend:wght@100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
     <link rel="stylesheet" href="css/partials.css">
     <link rel="stylesheet" href="./css/equipe.css">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Google+Sans:ital,opsz,wght@0,17..18,400..700;1,17..18,400..700&family=Lexend+Deca:wght@100..900&family=Lexend:wght@100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
-        rel="stylesheet">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=home" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    
     <link rel="icon" type="image/png" href="imagens/logosemfundo.png">
-    <title>Nossa Equipe</title>
 </head>
+
 <body>
     <header>
         <?php include 'partials/header.php'; ?>
     </header>
-    
+
     <section class="team-section">
         <h2 class="team-title">NOSSA <span>EQUIPE</span></h2>
-        
+
         <div class="team-grid">
-            
+
             <div class="team-card">
                 <div class="card-image-wrapper">
                     <img src="" alt="Eloah Barbosa" class="card-img">
@@ -37,10 +42,10 @@
                     <span class="badge-funcao">DEV FRONT-END</span>
                     <h3 class="dev-name-hover">Eloah Barbosa</h3>
                     <p class="dev-contribution">
-                        Responsável pela página Home, trabalhando na interface visual, organização dos 
+                        Responsável pela página Home, trabalhando na interface visual, organização dos
                         elementos e experiência do usuário dentro do site.
                     </p>
-                    
+
                 </div>
             </div>
 
@@ -60,7 +65,7 @@
                         funcionalidades relacionadas ao formulário de contratação, visualização de profissionais,
                         candidatura de profissionais e área de pagamento do sistema.
                     </p>
-                    
+
                 </div>
             </div>
 
@@ -81,8 +86,8 @@
                         e o admin.
                     </p>
                     <div class="social-links">
-                        
-            
+
+
                     </div>
                 </div>
             </div>
@@ -123,9 +128,9 @@
                     <p class="dev-contribution">
                         Responsável pelas paginas de acesso do sistema incluindo cadastro, login e edição de senha.
                         Também realizou a criação e organização do banco de dados do site, garantindo o funcionamento e
-                        armazenamento das informações do sistema. 
+                        armazenamento das informações do sistema.
                     </p>
-                    
+
                 </div>
             </div>
 
@@ -134,4 +139,5 @@
 
     <?php include 'partials/footer.php'; ?>
 </body>
+
 </html>
--- suporte.php
+++ suporte.php
@@ -43,6 +43,22 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link rel="stylesheet" href="css/suporte.css">
     <link rel="stylesheet" href="css/partials.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Google+Sans:ital,opsz,wght@0,17..18,400..700;1,17..18,400..700&family=Lexend+Deca:wght@100..900&family=Lexend:wght@100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=home" />
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Barlow+Semi+Condensed:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+    <link rel="icon" type="image/png" href="imagens/logosemfundo.png">
 </head>
 
 <body>
