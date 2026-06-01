74b2cedcf530434342c3bcbbdbb8981ec3e53a16
LUCAS VIEIRA ZANGRANDE <25254212@educ123.sp.senai.br>
2026-06-01 14:51:09 -0300

    Adicionei a verificação para exibir apenas os agendamentos correspondentes ao do profissional logado

---
+++ profissional/historicodeservicos.php
@@ -0,0 +1,76 @@
+<?php
+if (session_status() === PHP_SESSION_NONE){
+    session_start();
+}
+if (isset($_SESSION['mensagem'])) {
+
+    echo "
+    <script>
+        alert('{$_SESSION['mensagem']}');
+    </script>
+    ";
+
+    unset($_SESSION['mensagem']);
+}
+//adiciona o método de pagamento na tabela do sql, só exibe no detalhe de contratações
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Histórico de contratações</title>
+    <link rel="stylesheet" href="../css/userpage.css">
+    <link rel="stylesheet" href="../css/partials.css">
+</head>
+<?php
+require_once '../partials/header.php';
+?>
+<a href="./userpage.php">Voltar</a>
+<body>
+    <table>
+        <tr>
+            <!--<th colspan="99">Histórico de contratações<th>-->
+            <th>Data</th>
+            <th>Tempo estimado</th>
+            <th>Valor</th>
+            <th>Descrição</th>
+            <th>Endereço</th>
+            <th>Contratante</th>
+            <th>Profissional</th>
+            <th>Status</th>
+            <th class='td_verDetalhes'></th>
+        </tr>
+<?php
+require_once '../crud.php';
+
+$tableAgenda = readAll($pdo,'agenda');
+        foreach($tableAgenda as $agendamento){
+
+    $nomeProfi = read_nome_via_ID($pdo,'usuarios',$agendamento['id_profissional']);
+    $nomeCliente = read_nome_via_ID($pdo,'usuarios',$agendamento['id_cliente']);
+$palavras = explode(' ', trim($agendamento['descricao_problema'])); 
+    
+    $descricaoResumida = (count($palavras) > 4) 
+        ? implode(' ', array_slice($palavras, 0, 4)) . '...' 
+        : $agendamento['descricao_problema'];
+        if($agendamento['id_profissional'] === $_SESSION['id_user']){
+            echo "<tr>
+                    <td>".$agendamento['data']."</td>
+                    <td>".$agendamento['tempo_planejado']."</td>
+                    <td>".$agendamento['valor_total']."</td>
+                    <td>".$descricaoResumida."</td>
+                    <td>".$agendamento['endereco_servico']."</td>
+                    <td>".$nomeCliente."</td>
+                    <td>".$nomeProfi."</td>
+                    <td>".$agendamento['status_os']."</td>
+                    <td class='td_verDetalhes'><a class='verDetalhes' href='detalhesContratacao.php?id=".$agendamento['id_os']."'>Ver detalhes</a></td>";
+        }
+    }
+    echo "</tr>";
+?>
+</table>
+
+</body>
+</html>
