a1ada5f6ba5909040cb58adf9e4f78c71cdcee80
guiisilva00 <guilherme.p.silva51@aluno.senai.br>
2026-06-03 17:02:22 -0300

    Criação do arquivo responder saq

---
+++ admin/resposta_suporte.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Responder SAQ | Sync</title>
+    <link rel="stylesheet" href="../css/partials.css">
+    <link rel="stylesheet" href="../css/formularios.css">
+</head>
+<body>
+    
+</body>
+</html>
